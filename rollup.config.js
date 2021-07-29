a9bcf1996a3df51b617e663e879d55450606f603
al-chaudhari <chaudharia041@gmail.com>
2021-07-29 17:15:59 +0530

    chore: rollup

---
+++ rollup.config.js
@@ -0,0 +1,51 @@
+export default [
+  {
+    input: "src/constant.js",
+    output: {
+      file: "./swiss_accounting_integration/public/js/constant.js",
+      format: "iife",
+    },
+  },
+  {
+    input: "src/createqrbill.js",
+    output: {
+      file: "./swiss_accounting_integration/public/js/createqrbill.js",
+      format: "iife",
+    },
+  },
+  {
+    input: "src/generateqrpdf.js",
+    output: {
+      file: "./swiss_accounting_integration/public/js/generateqrpdf.js",
+      format: "iife",
+    },
+  },
+  {
+    input: "src/index.js",
+    output: {
+      file: "./swiss_accounting_integration/public/js/index.js",
+      format: "iife",
+    },
+  },
+  {
+    input: "src/message.js",
+    output: {
+      file: "./swiss_accounting_integration/public/js/message.js",
+      format: "iife",
+    },
+  },
+  {
+    input: "src/qrconfig.js",
+    output: {
+      file: "./swiss_accounting_integration/public/js/qrconfig.js",
+      format: "iife",
+    },
+  },
+  {
+    input: "src/utils.js",
+    output: {
+      file: "./swiss_accounting_integration/public/js/utils.js",
+      format: "iife",
+    },
+  },
+];
