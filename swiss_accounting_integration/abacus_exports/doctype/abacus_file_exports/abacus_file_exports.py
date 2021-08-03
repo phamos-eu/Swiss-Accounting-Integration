039d64d5b54cc8475bda0bb62eb8b43c7cda4257
al-chaudhari <chaudharia041@gmail.com>
2021-08-03 19:33:19 +0530

    added ORM for Single SQL

--- swiss_accounting_integration/abacus_exports/doctype/abacus_file_exports/abacus_file_exports.py
+++ swiss_accounting_integration/abacus_exports/doctype/abacus_file_exports/abacus_file_exports.py
@@ -3,6 +3,7 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
+import frappe
 # import frappe
 from frappe.model.document import Document
 
@@ -19,6 +20,92 @@ class AbacusFileExports(Document):
 
     def get_transactions(self):
         """
-        What is Transations 
+        What are Transactions ?
+
+        - Sales Invoice
+        - Purchase Invoice
+        - Payment Entry
+        - Journal Entry
         """
+        # Get Sales Invoice
+        sales_invoices = frappe.db.get_list('Sales Invoice',
+                                            filters={
+                                                'posting_date': self.start_date,
+                                                'posting_date': self.end_date,
+                                                'docstatus': 1,
+                                                'exported_to_abacus': 0,
+                                                'company': self.Company
+
+                                            })
+
+        # Get Purchase Invoice
+        purchase_invoice = frappe.db.get_list('Purchase Invoice',
+                                              filters={
+                                                  'posting_date': self.start_date,
+                                                  'posting_date': self.end_date,
+                                                  'docstatus': 1,
+                                                  'exported_to_abacus': 0,
+                                                  'company': self.Company
+                                              })
+        # Get Payment Entry
+        payment_entry = frappe.db.get_list('Payment Entry',
+                                           filters={
+                                               'posting_date': self.start_date,
+                                               'posting_date': self.end_date,
+                                               'docstatus': 1,
+                                               'exported_to_abacus': 0,
+                                               'company': self.Company
+                                           })
+        # Get Journal Entry
+        journal_entry = frappe.db.get_list('Journal Entry',
+                                           filters={
+                                               'posting_date': self.start_date,
+                                               'posting_date': self.end_date,
+                                               'docstatus': 1,
+                                               'exported_to_abacus': 0,
+                                               'company': self.Company
+                                           })
+
+        # Combining Entry
+        docs = sales_invoices + purchase_invoice + payment_entry + journal_entry
+
+        # clear all children
+        self.references = []
+
+        # add to child table
+        for doc in docs:
+            self.append('references', {'dt': doc['dt'], 'dn': doc['dn']})
+        self.save()
+
+        # mark as exported
+        sinvs = self.get_docs(docs, "Sales Invoice")
+        pinvs = self.get_docs(docs, "Purchase Invoice")
+        pes = self.get_docs(docs, "Payment Entry")
+        jvs = self.get_docs(docs, "Journal Entry")
+
+        update_query = """UPDATE `tabSales Invoice`
+                SET `tabSales Invoice`.`exported_to_abacus` = 1
+                WHERE
+                    `tabSales Invoice`.`name` IN ({sinvs});""".format(
+            sinvs=self.get_sql_list(sinvs))
+        frappe.db.sql(update_query)
+        update_query = """UPDATE `tabPurchase Invoice`
+                SET `tabPurchase Invoice`.`exported_to_abacus` = 1
+                WHERE
+                    `tabPurchase Invoice`.`name` IN ({pinvs});""".format(
+            pinvs=self.get_sql_list(pinvs))
+        frappe.db.sql(update_query)
+        update_query = """UPDATE `tabPayment Entry`
+                SET `tabPayment Entry`.`exported_to_abacus` = 1
+                WHERE
+                    `tabPayment Entry`.`name` IN ({pes});""".format(
+            pes=self.get_sql_list(pes))
+        frappe.db.sql(update_query)
+        update_query = """UPDATE `tabJournal Entry`
+                SET `tabJournal Entry`.`exported_to_abacus` = 1
+                WHERE
+                    `tabJournal Entry`.`name` IN ({jvs});""".format(
+            jvs=self.get_sql_list(jvs))
+        frappe.db.sql(update_query)
+        return
         pass
