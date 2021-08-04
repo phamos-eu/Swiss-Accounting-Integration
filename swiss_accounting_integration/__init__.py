30f50445f21da162d3f63a4cf433b59cf94617ff
al-chaudhari <chaudharia041@gmail.com>
2021-08-04 21:15:34 +0530

    added method for whitelisted

--- swiss_accounting_integration/__init__.py
+++ swiss_accounting_integration/__init__.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import frappe
+from .utils import get_aggregated_transaction, get_individual_transation
+import frappe
+
 __version__ = '0.0.1'
 
+
+@frappe.whitelist()
+def create_abacus_export(start_date, end_date, is_aggregated):
+    """
+    return xml Template to Download  
+    if Aggregated is Required ?
+    """
+    if is_aggregated:
+        return get_aggregated_transaction(start_date, end_date)
+    else:
+        return get_individual_transation(start_date, end_date)
