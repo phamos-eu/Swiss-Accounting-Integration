84a2dba1a3a0998e821af05912de840c3080eba8
al-chaudhari <chaudharia041@gmail.com>
2021-08-10 20:53:43 +0530

    added all entry data

--- swiss_accounting_integration/__init__.py
+++ swiss_accounting_integration/__init__.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 import frappe
 from .utils import get_aggregated_transaction, get_individual_transation
 import frappe
+import datetime
 
 __version__ = '0.0.1'
 
@@ -11,7 +12,7 @@ __version__ = '0.0.1'
 @frappe.whitelist()
 def create_abacus_export(start_date, end_date, is_aggregated):
     """
-    return xml Template to Download  
+    return xml Template to Download
     if Aggregated is Required ?
     """
     if is_aggregated:
@@ -23,7 +24,8 @@ def create_abacus_export(start_date, end_date, is_aggregated):
 @frappe.whitelist()
 def gl():
     """
-    The Getting the GL Entry and Creating the Data Structure
+    The Getting the GL Entry and Creating
+    the Data Structure
     """
 
     entries = frappe.db.get_list('GL Entry',
@@ -31,7 +33,6 @@ def gl():
                                  filters={
                                      'posting_date': ['>=', '08-09-2021']
                                  })
-
     for entry in entries:
         entryLevel = 'A'
         entryType = 'S'
@@ -78,3 +79,128 @@ def gl():
         'taxCode': 111,
         'flatRate': 0
     }
+
+
+@frappe.whitelist()
+def gl2():
+    """
+    Aggregation Not Working
+    """
+
+    transactions = []
+    # TODO: Get Currency From Options
+    company = 'Grynn Advanced'
+    baseCurrency = frappe.get_value('Company', company, 'default_currency')
+
+    invoices = frappe.get_list('Sales Invoice', filters={
+        'posting_date': ['>=', '08-09-2021']
+    })
+
+    for invoice in invoices:
+        inv = frappe.get_doc('Sales Invoice', invoice.name)
+        if inv.taxes_and_charges:
+            tax_record = frappe.get_doc(
+                "Sales Taxes and Charges Template", inv.taxes_and_charges)
+            tax_code = 312  # tax_record.tax_code
+            rate = tax_record.taxes[0].rate
+            taxAccount = tax_record.taxes[0].account_head
+        else:
+            tax_code = None
+
+        for item in inv.items:
+            transactions.append({
+                'account': getAccountNumber(inv.debit_to),
+                'amount': inv.base_grand_total,
+                'singles': [{
+                    'account':  getAccountNumber(item.income_account),
+                    'amount': inv.base_net_total,
+                    'currency': inv.currency
+                }],
+                'debit_credit': 'D',
+                'date': datetime.datetime.now(),
+                'currency': inv.currency,
+                'tax_account':   getAccountNumber(taxAccount) or None,
+                'tax_amount': inv.total_taxes_and_charges or None,
+                'tax_rate': rate or None,
+                'tax_code': tax_code or "312",
+                'tax_currency': baseCurrency,
+                'text1': inv.name
+            })
+
+    # Purchase Invoice
+
+    purchaseInvoices = frappe.get_list('Purchase Invoice', filters={
+        'posting_date': ['>=', '08-09-2021']
+    })
+
+    for invoice in purchaseInvoices:
+        inv = frappe.get_doc('Purchase Invoice', invoice.name)
+        if inv.taxes_and_charges:
+            tax_record = frappe.get_doc(
+                "Sales Taxes and Charges Template", inv.taxes_and_charges)
+            tax_code = 312  # tax_record.tax_code
+            rate = tax_record.taxes[0].rate
+            taxAccount = tax_record.taxes[0].account_head
+        else:
+            tax_code = None
+
+        for item in inv.items:
+            transactions.append({
+                'account': getAccountNumber(inv.credit_to),
+                'amount': inv.base_grand_total,
+                'singles': [{
+                    'account':  getAccountNumber(item.expense_account),
+                    'amount': inv.base_net_total,
+                    'currency': inv.currency
+                }],
+                'debit_credit': 'C',
+                'date': datetime.datetime.now(),
+                'currency': inv.currency,
+                'tax_account':   getAccountNumber(taxAccount) or None,
+                'tax_amount': inv.total_taxes_and_charges or None,
+                'tax_rate': rate or None,
+                'tax_code': tax_code or "312",
+                'tax_currency': baseCurrency,
+                'text1': inv.name
+            })
+
+    # Payment Entry
+
+    paymentEntry = frappe.get_list('Purchase Invoice', filters={
+        'posting_date': ['>=', '08-09-2021']
+    })
+
+    for invoice in paymentEntry:
+        inv = frappe.get_doc('Payment Entry', invoice.name)
+
+        for item in inv.items:
+            transactions.append({
+                'account': getAccountNumber(inv.paid_from),
+                'amount': inv.paid_amount,
+                'singles': [{
+                    'account':  getAccountNumber(item.paid_to),
+                    'amount': inv.paid_amount,
+                    'currency': inv.paid_to_account_currency
+                }],
+                'debit_credit': 'C',
+                'date': datetime.datetime.now(),
+                'currency': inv.paid_from_account_currency,
+                'tax_account': None,
+                'tax_amount': None,
+                'tax_rate': None,
+                'tax_code': None,
+                'text1': inv.name
+            })
+
+    return transactions
+
+
+def getAccountNumber(account_name):
+    """
+    Get Account Number
+    or None
+    """
+    if account_name:
+        return frappe.get_value('Account', account_name, 'account_number')
+    else:
+        return None
