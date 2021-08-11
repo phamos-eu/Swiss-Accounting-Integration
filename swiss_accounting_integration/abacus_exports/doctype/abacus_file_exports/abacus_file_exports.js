9ba867b152f64a58a4b28f1dee421864af74f1d6
al-chaudhari <chaudharia041@gmail.com>
2021-08-11 19:11:58 +0530

    fix: removed abacus file exports

--- swiss_accounting_integration/abacus_exports/doctype/abacus_file_exports/abacus_file_exports.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2021, Grynn and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Abacus File Exports', {
-	// refresh: function(frm) {
-
-	// }
-});
