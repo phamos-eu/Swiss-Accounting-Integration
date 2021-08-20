d8f8dd12a1962f819c204dbe078f4e6bcd282a6a
al-chaudhari <chaudharia041@gmail.com>
2021-08-20 19:28:20 +0530

    field: attach File

--- swiss_accounting_integration/abacus_exports/doctype/abacus_export/abacus_export.js
+++ swiss_accounting_integration/abacus_exports/doctype/abacus_export/abacus_export.js
@@ -6,3 +6,24 @@ frappe.ui.form.on('Abacus Export', {
 
 	// }
 });
+const attachFile = (doctype, docname, file, frm) => {
+  let formData = new FormData();
+  formData.append("is_private", 1);
+  formData.append("folder", "Home/Attachments");
+  formData.append("doctype", doctype);
+  formData.append("docname", docname);
+  formData.append("file", new Blob([file], { type: "text/xml" }), "abacus.xml");
+
+  showProgress(60, "Uploading File", "Uploading File");
+  fetch(FRAPPE_FILE_UPLOAD_ENDPOINT, {
+    headers: {
+      Accept: "application/json",
+      "X-Frappe-CSRF-Token": window.frappe.csrf_token,
+    },
+    method: "POST",
+    body: formData,
+  }).then(() => {
+    showProgress(100, "done", "done");
+    frm.reload_doc();
+  });
+};
