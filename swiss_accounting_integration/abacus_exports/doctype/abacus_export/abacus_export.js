90bf187785b12b4873a7201ba45eae68f41e01a2
al-chaudhari <chaudharia041@gmail.com>
2021-08-20 19:31:30 +0530

    feat: add chunk

--- swiss_accounting_integration/abacus_exports/doctype/abacus_export/abacus_export.js
+++ swiss_accounting_integration/abacus_exports/doctype/abacus_export/abacus_export.js
@@ -1,10 +1,33 @@
 // Copyright (c) 2021, Grynn and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Abacus Export', {
-	// refresh: function(frm) {
+const FRAPPE_FILE_UPLOAD_ENDPOINT = "/api/method/upload_file";
 
-	// }
+frappe.ui.form.on("Abacus Export", {
+  refresh: (frm) => {
+    if (frm.doc.docstatus == 1) {
+      frm.add_custom_button("Re Export", function () {
+        const start_date = frm.doc.start_date;
+        const end_date = frm.doc.end_date;
+        const company = frm.doc.company;
+        const doctype = frm.doctype;
+        const docname = frm.docname;
+        showProgress(20, "Getting File", "Getting File");
+        fetch(
+          encodeURI(
+            `/api/method/swiss_accounting_integration.reset_account?start_date=${start_date}&end_date=${end_date}&company=${company}&doctype=${doctype}&docname=${docname}`
+          )
+        )
+          .then((data) => data.json())
+          .then((data) => data.message.file)
+          .then((file) => attachFile(doctype, docname, file, frm))
+          .catch((err) => {
+            window.frappe.hide_progress();
+            window.frappe.throw(err + "");
+          });
+      });
+    }
+  },
 });
 
 const showProgress = (current, description) => {
