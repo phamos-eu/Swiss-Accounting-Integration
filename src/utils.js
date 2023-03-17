752a438c9e1b9a39f3f6edf0eb2d38c5f28cfdae
estackhub <spryng.managed@gmail.com>
2023-03-17 22:10:49 +0000

    lang check fix

--- src/utils.js
+++ src/utils.js
@@ -101,9 +101,9 @@ export const getLanguageCode = (language) => {
   }
 
   if (
-    (language === "en") |
-    (language === "fr") |
-    (language === "it") |
+    (language === "en") ||
+    (language === "fr") ||
+    (language === "it") ||
     (language === "de")
   ) {
     return language.toUpperCase();
